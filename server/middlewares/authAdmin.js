8f38d6bf97aa0bd9bde10e13fb03a53ec3af63db
emperorbj <cwaku96@gmail.com>
2025-01-27 15:31:30 -0800

    Enhance admin token validation in authAdmin middleware to verify email against environment variable

--- server/middlewares/authAdmin.js
+++ server/middlewares/authAdmin.js
@@ -5,6 +5,10 @@ export const authAdmin = (req,res,next) => {
         const {adminToken} = req.headers;
         if(!adminToken) return res.status(401).json
         ({ success:false, message:"Access denied due to invalid token" });
+
+        const decoded = jwt.verify(adminToken, process.env.JWT_SECRET);
+        if(decoded.email !== process.env.ADMIN_EMAIL) return res.status(401).json
+        ({ success:false, message:"Access denied" });     
     } catch (error) {
         console.log(error);
         res.status(500).json({ success:false, message:error.message });
