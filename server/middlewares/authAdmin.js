fcf745b33e3bf6810e463c7196a8634a0b8b1604
emperorbj <cwaku96@gmail.com>
2025-01-27 15:30:36 -0800

    Add admin token validation to authAdmin middleware

--- server/middlewares/authAdmin.js
+++ server/middlewares/authAdmin.js
@@ -2,7 +2,9 @@ import jwt from 'jsonwebtoken';
 
 export const authAdmin = (req,res,next) => {
     try {
-        
+        const {adminToken} = req.headers;
+        if(!adminToken) return res.status(401).json
+        ({ success:false, message:"Access denied due to invalid token" });
     } catch (error) {
         console.log(error);
         res.status(500).json({ success:false, message:error.message });
